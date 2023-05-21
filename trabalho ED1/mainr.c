ef0938275daa2ceb3e25aa57bf0ee2a25e5e71ce
ryansarcinelli <114365610+ryansarcinelli@users.noreply.github.com>
2023-05-21 15:29:17 -0300

    ADD alocameio

--- trabalho ED1/mainr.c
+++ trabalho ED1/mainr.c
@@ -3,34 +3,42 @@
 #include <string.h>
 #define v 50
 
-typedef struct Descritor {
+typedef struct Meio {
     struct Filme* filme;
-    struct Descritor* prox;
+    struct Meio* prox;
 } Meio;
 
-typedef struct ListaDup {
-    struct ListaDup* prox;
-    struct ListaDup* ant;
+typedef struct Seq {
+    struct Seq* prox;
+    struct Seq* ant;
     struct Filme* filme;
 } Seq;
 
 typedef struct Filme {
     int id, ano;
     char nome[v];
-    struct ListaDup* seq;
+    struct Seq* seq;
 } Filme;
 
-typedef struct Desc {
+typedef struct Topo {
     int n;
-    struct Desc* prim;
-    struct Desc* ult;
+    struct Meio* prim;
+    struct Meio* ult;
 } Topo;
 
 //Essa função aloca espaço para um novo filme.
 Filme* criarFilme() {
     Filme* f = (Filme*)malloc(sizeof(Filme));
+    if (f == NULL){
+        printf("Erro ao alocar memória para o filme.\n");
+        exit(1);
+    }
     printf("\nDigite o nome do filme: ");
-    fgets(f->nome, v, stdin);
+    if (fgets(f->nome, v, stdin) == NULL) {
+        printf("Erro ao ler o nome do filme.\n");
+        free(f);
+        exit(1);
+    }
     f->nome[strcspn(f->nome, "\n")] = '\0';
 
     printf("\nDigite o ano: ");
@@ -44,36 +52,44 @@ Filme* criarFilme() {
     x++;
     return f;
 }
-
+int FilmeVazia(Filme *f){
+    if(f->nome==NULL){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 //Essa função imprime os dados de um filme.
 void imprimirFilme(Filme* f) {
     printf("\nNome: %s | Ano: %d | Id: %d", f->nome, f->ano,f->id);
 }
-
 void alterarNome(Filme* f) {
     int num;
     printf("\nDigite o ID do filme que deseja alterar o nome: ");
     scanf("%d", &num);
-    fflush(stdin);
+    getcehr();
     for (Filme* p = f; p != NULL; p = p->seq->filme) {
         if (num == p->id) {
             char aux[v];
-            printf("\nDigite o novo nome do filme %d: ", num);
-            fgets(aux, v, stdin);
-            aux[strcspn(aux, "\n")] = '\0';
-            fflush(stdin);
+            printf("\nDigite o nome do filme: ");
+            if (fgets(f->nome, v, stdin) == NULL) {
+                printf("Erro ao ler o nome do filme.\n");
+                free(f);
+                exit(1);
+            }
+            f->nome[strcspn(f->nome, "\n")] = '\0';
+            getchar();
             strcpy(p->nome, aux);
             return;
-        } else {
+        } else
             printf("\nFilme nao encontrado ou nao existe.");
-        }
     }
 }
 void alterarAno(Filme* f) {
     int num;
     printf("\nDigite o ID do filme que deseja alterar o ano: ");
     scanf("%d", &num);
-    fflush(stdin);
+    getchar();
     for (Filme* p=f;p!=NULL;p=p->seq->filme) {
         if (num == p->id) {
             int aux;
@@ -85,19 +101,27 @@ void alterarAno(Filme* f) {
             printf("\nFilme nao encontrado ou nao existe.");
     }
 }
-
 int retornaAno(Filme* f){
     int num;
     printf("\nDigite o ID do filme que deseja saber o ano: ");
     scanf("%d", &num);
-    fflush(stdin);
+    getchar();
     for (Filme* p=f;p!=NULL;p=p->seq->filme){
         if (num == p->id) {
             return p->ano;
         } else
             printf("\nFilme nao encontrado ou nao existe.");
-        
     }
+}
+Meio* alocaMeio(Filme* f){
+    Meio* m = (Meio*)malloc(sizeof(Meio));
+    if (m == NULL){
+        printf("Erro ao alocar memória para o meio.\n");
+        exit(1);
+    }
+    m->filme = f;
+    m->prox = NULL;
+    return m;
 }
 int main(){
     Filme* f = criarFilme();
@@ -108,6 +132,7 @@ int main(){
     //imprimirFilme(f);
     //int ano=retornaAno(f);
     //printf("\nAno do filme %d: %d", f->id, ano);
+    Meio*m=alocaMeio(f);
     free(f);
     return 0;
 }

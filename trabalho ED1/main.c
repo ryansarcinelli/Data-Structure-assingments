68eae7bd9b8f3b3af31854b4aaf492ce861d5377
ryansarcinelli <114365610+ryansarcinelli@users.noreply.github.com>
2023-05-23 23:04:46 -0300

    ADD muito

--- trabalho ED1/main.c
+++ trabalho ED1/main.c
@@ -27,29 +27,13 @@ typedef struct Topo {
 } Topo;
 
 //Essa função aloca espaço para um novo filme.
-Filme* criarFilme() {
+Filme* alocaFilme() {
     Filme* f = (Filme*)malloc(sizeof(Filme));
     if (f == NULL) {
         printf("Erro ao alocar memória para o filme.\n");
         exit(1);
     }
-    printf("\nDigite o nome do filme: ");
-    if (fgets(f->nome, v, stdin) == NULL) {
-        printf("Erro ao ler o nome do filme.\n");
-        free(f);
-        exit(1);
-    }
-    f->nome[strcspn(f->nome, "\n")] = '\0';
-
-    printf("\nDigite o ano: ");
-    scanf("%d", &f->ano);
-    getchar();
-
     f->seq = NULL;
-
-    static int x = 1;
-    f->id = x;
-    x++;
     return f;
 }
 //Essa função imprime os dados de um filme.
@@ -116,27 +100,34 @@ Meio* alocaMeio(Filme* f) {
     m->prox = NULL;
     return m;
 }
-Meio* adicionarFilmeOrdenado(Meio* m, Filme* f) {
-    Meio* novo = alocaMeio(f);
 
-    if (m == NULL) {
-        return novo;
+void inserirFilmeOrdenado(Meio** descritor, Filme* novoFilme) {
+    Meio* novoMeio = (Meio*)malloc(sizeof(Meio));
+    novoMeio->filme = novoFilme;
+    novoMeio->prox = NULL;
+
+    if (*descritor == NULL || strcmp(novoFilme->nome, (*descritor)->filme->nome) < 0) {
+        // Inserir no início da lista
+        novoMeio->prox = *descritor;
+        *descritor = novoMeio;
+        (*descritor)->filme->id = 1; // Atualizar o ID do primeiro filme
+    } else {
+        Meio* atual = *descritor;
+        while (atual->prox != NULL && strcmp(novoFilme->nome, atual->prox->filme->nome) > 0) {
+            atual = atual->prox;
+        }
+        novoMeio->prox = atual->prox;
+        atual->prox = novoMeio;
+
+        // Atualizar os IDs dos filmes após a inserção
+        Meio* temp = novoMeio->prox;
+        int novoId = novoMeio->filme->id + 1;
+        while (temp != NULL) {
+            temp->filme->id = novoId;
+            novoId++;
+            temp = temp->prox;
+        }
     }
-
-    Meio* atual = m;
-    Meio* anterior = NULL;
-
-    // Percorre a lista para encontrar a posição correta de inserção
-    while (atual != NULL && strcmp(f->nome, atual->filme->nome) > 0) {
-        anterior = atual;
-        atual = atual->prox;
-    }
-
-    // Insere o novo filme na posição correta
-    anterior->prox = novo;
-    novo->prox = atual;
-
-    return m;
 }
 void exibirLista(Meio* m) {
     printf("Lista de Filmes:\n");
@@ -144,27 +135,126 @@ void exibirLista(Meio* m) {
         printf("Nome: %s | Ano: %d | Id: %d\n", atual->filme->nome, atual->filme->ano, atual->filme->id);
     }
 }
-void adicionarFilmeUsuario(Meio* m) {
-    Filme* novoFilme = criarFilme();
-    m = adicionarFilmeOrdenado(m, novoFilme);
+void adicionarFilmeUsuario(Meio** m) {
+    Filme* novoFilme = alocaFilme();
+    inserirFilmeOrdenado(m, novoFilme);
     printf("\nFilme adicionado com sucesso.");
 }
 
-int main(){
-    //alterarNome(f);
-    //imprimirFilme(f);
-    //alterarAno(f);
-    //imprimirFilme(f);
-    //int ano=retornaAno(f);
-    //printf("\nAno do filme %d: %d", f->id, ano);
-    Filme* f = criarFilme();
-    Meio* m = alocaMeio(f);
-    //adicionarFilmeUsuario(m);
-    //exibirLista(m);
-    imprimirFilme(f);
-    alteradado(m,f);
-    imprimirFilme(f);
-    free(m);
-    free(f);
+void removerFilme(Meio** m) {
+    int num;
+    printf("\nDigite o ID do filme que deseja remover: ");
+    scanf("%d", &num);
+    getchar();
+
+    Meio* anterior = NULL;
+
+    for (Meio* atual = *m; atual != NULL; anterior = atual, atual = atual->prox) {
+        if (num == atual->filme->id) {
+            // Remover o filme da sequência
+            Seq* seqAtual = atual->filme->seq;
+            while (seqAtual != NULL) {
+                Seq* seqProx = seqAtual->prox;
+                free(seqAtual);
+                seqAtual = seqProx;
+            }
+
+            // Remover o filme da lista principal
+            if (anterior == NULL) {
+                *m = atual->prox;
+            } else {
+                anterior->prox = atual->prox;
+            }
+
+            free(atual->filme);
+            free(atual);
+            printf("\nFilme removido com sucesso.");
+            return;
+        }
+    }
+
+    printf("\nFilme nao encontrado ou nao existe.");
+}
+
+void liberarMemoria(Topo* topo) {
+    Meio* atual = topo->prim;
+    while (atual != NULL) {
+        Filme* filme = atual->filme;
+
+        // Liberar a sequência do filme
+        Seq* seqAtual = filme->seq;
+        while (seqAtual != NULL) {
+            Seq* seqProx = seqAtual->prox;
+            free(seqAtual);
+            seqAtual = seqProx;
+        }
+
+        // Liberar o filme
+        free(filme);
+
+        // Avançar para o próximo meio
+        Meio* prox = atual->prox;
+        free(atual);
+        atual = prox;
+    }
+
+    // Reinicializar o descritor
+    topo->prim = NULL;
+    topo->ult = NULL;
+    topo->n = 0;
+}
+void clearScreen() {
+    // Limpa a tela do console
+    printf("\033[2J\033[1;1H");
+}
+
+void exibirMenu() {
+    printf("\n\nSelecione uma opcao:\n");
+    printf("1 - Inserir novo filme\n");
+    printf("2 - Alterar dados de um filme\n");
+    printf("3 - Remover filme\n");
+    printf("4 - Imprimir lista de filmes\n");
+    printf("0 - Sair\n");
+    printf("Opcao: ");
+}
+
+int main() {
+    Topo topo;
+    topo.n = 0;
+    topo.prim = NULL;
+    topo.ult = NULL;
+
+    int opcao;
+    do {
+        exibirMenu();
+        scanf("%d", &opcao);
+        getchar(); // Limpar o caractere de nova linha do buffer
+
+        switch (opcao) {
+            case 1:
+                adicionarFilmeUsuario(&topo.prim);
+                break;
+            case 2:
+                // Opção 2: Alterar dados de um filme
+                alteradado(topo.prim, topo.prim->filme);
+                break;
+            case 3:
+                // Opção 3: Remover filme
+                removerFilme(&topo.prim);
+                break;
+            case 4:
+                // Opção 4: Imprimir lista de filmes
+                exibirLista(topo.prim);
+                break;
+            case 0:
+                // Opção 0: Sair
+                printf("Saindo do programa...\n");
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+                break;
+        }
+    } while (opcao != 0);
+    liberarMemoria(&topo);
     return 0;
 }
